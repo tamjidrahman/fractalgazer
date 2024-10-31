a3ec763f4e3ce3fd722fcae51161580dd9ac6ea3
Tamjid Rahman <tamjidarrahman@gmail.com>
2024-10-31 00:10:25 -0400

    refactor

--- main.go
+++ main.go
@@ -16,26 +16,35 @@ const (
 	MAX_ITER     = 100
 )
 
-func getColor(index int) color.Color {
-	if index == MAX_ITER {
-		return color.Black
+var colorCache [MAX_ITER + 1]color.RGBA
+
+func init() {
+	for i := 0; i <= MAX_ITER; i++ {
+		if i == MAX_ITER {
+			colorCache[i] = color.RGBA{0, 0, 0, 255}
+		} else {
+			t := float64(i) / float64(MAX_ITER)
+			r := uint8(9 * (1 - t) * t * t * t * 255)
+			g := uint8(15 * (1 - t) * (1 - t) * t * t * 255)
+			b := uint8(8.5 * (1 - t) * (1 - t) * (1 - t) * t * 255)
+			colorCache[i] = color.RGBA{r, g, b, 255}
+		}
 	}
+}
 
-	t := float64(index) / float64(MAX_ITER)
-	r := uint8(9 * (1 - t) * t * t * t * 255)
-	g := uint8(15 * (1 - t) * (1 - t) * t * t * 255)
-	b := uint8(8.5 * (1 - t) * (1 - t) * (1 - t) * t * 255)
-
-	return color.RGBA{r, g, b, 255}
+func getColor(index int) color.RGBA {
+	return colorCache[index]
 }
 
 type Game struct {
-	fps   float64
-	xMin  float64
-	xMax  float64
-	yMin  float64
-	yMax  float64
-	scale float64
+	fps        float64
+	xMin       float64
+	xMax       float64
+	yMin       float64
+	yMax       float64
+	scale      float64
+	linearStep float64
+	zoomSpeed  float64
 }
 type Coord = float64
 type PixelCoord = int
@@ -44,23 +53,23 @@ func (g *Game) Update() error {
 	g.fps = ebiten.ActualFPS()
 
 	if ebiten.IsKeyPressed(ebiten.KeyA) {
-		g.xMin -= 0.1 * g.scale
-		g.xMax -= 0.1 * g.scale
+		g.xMin -= g.linearStep * g.scale
+		g.xMax -= g.linearStep * g.scale
 	}
 	if ebiten.IsKeyPressed(ebiten.KeyD) {
-		g.xMin += 0.1 * g.scale
-		g.xMax += 0.1 * g.scale
+		g.xMin += g.linearStep * g.scale
+		g.xMax += g.linearStep * g.scale
 	}
 	if ebiten.IsKeyPressed(ebiten.KeyW) {
-		g.yMin += 0.1 * g.scale
-		g.yMax += 0.1 * g.scale
+		g.yMin += g.linearStep * g.scale
+		g.yMax += g.linearStep * g.scale
 	}
 	if ebiten.IsKeyPressed(ebiten.KeyS) {
-		g.yMin -= 0.1 * g.scale
-		g.yMax -= 0.1 * g.scale
+		g.yMin -= g.linearStep * g.scale
+		g.yMax -= g.linearStep * g.scale
 	}
 	if ebiten.IsKeyPressed(ebiten.KeyQ) {
-		g.scale *= 0.95
+		g.scale *= (1.0 - g.zoomSpeed)
 		yMid := (g.yMin + g.yMax) / 2
 		xMid := (g.xMin + g.xMax) / 2
 		g.xMin = xMid - g.scale
@@ -69,7 +78,7 @@ func (g *Game) Update() error {
 		g.yMax = yMid + g.scale
 	}
 	if ebiten.IsKeyPressed(ebiten.KeyE) {
-		g.scale *= 1.05
+		g.scale *= (1.0 + g.zoomSpeed)
 		yMid := (g.yMin + g.yMax) / 2
 		xMid := (g.xMin + g.xMax) / 2
 		g.xMin = xMid - g.scale
@@ -115,14 +124,19 @@ func (p *PixelPoint) toPoint(g *Game) Point {
 }
 
 func getTValue(p *Point) int {
-	x0, y0 := p.X, p.Y
-	x, y := 0.0, 0.0
+	x, y := p.X, p.Y
+	x2, y2 := x*x, y*y
 	iteration := 0
-	for x*x+y*y <= 4 && iteration < MAX_ITER {
-		xtemp := x*x - y*y + x0
-		y = 2*x*y + y0
-		x = xtemp
+	for x2+y2 <= 4 && iteration < MAX_ITER {
+		y = 2*x*y + p.Y
+		x = x2 - y2 + p.X
+		x2, y2 = x*x, y*y
 		iteration++
+
+		// Early escape optimization
+		if x2+y2 > 4 {
+			break
+		}
 	}
 	return iteration
 }
@@ -131,14 +145,17 @@ var wg sync.WaitGroup
 
 var t_values = [screenWidth][screenHeight]int{}
 
-func getTValueChunked(g *Game, startY, endY int) {
+func getTValueChunked(g *Game, startY, endY int, result []int) {
 	defer wg.Done()
-	for x := 0; x < screenWidth; x++ {
-		for y := startY; y < endY; y++ {
-			p := PixelPoint{X: x, Y: y}
-			point := p.toPoint(g)
-			t_value := getTValue(&point)
-			t_values[x][y] = t_value
+	for y := startY; y < endY; y++ {
+		for x := 0; x < screenWidth; x++ {
+			point := Point{
+				X: g.xMin + (g.xMax-g.xMin)/screenWidth*float64(x),
+				Y: g.yMax - (g.yMax-g.yMin)/screenHeight*float64(y),
+			}
+
+			result[y*screenWidth+x] = getTValue(&point)
+
 		}
 	}
 }
@@ -146,30 +163,38 @@ func getTValueChunked(g *Game, startY, endY int) {
 func colorCanvas(g *Game, screen *ebiten.Image) {
 	numGoroutines := runtime.NumCPU()
 	chunkSize := screenHeight / numGoroutines
+	result := make([]int, screenWidth*screenHeight)
 
 	for i := 0; i < numGoroutines; i++ {
 		wg.Add(1)
-		go getTValueChunked(g, i*chunkSize, (i+1)*chunkSize)
+		go getTValueChunked(g, i*chunkSize, (i+1)*chunkSize, result)
 	}
 
 	wg.Wait()
 
-	for x := 0; x < screenWidth; x++ {
-		for y := 0; y < screenHeight; y++ {
-			screen.Set(x, y, getColor(t_values[x][y]))
-		}
+	pixels := make([]byte, screenWidth*screenHeight*4)
+	for i, v := range result {
+		color := getColor(v)
+		pixels[i*4] = color.R
+		pixels[i*4+1] = color.G
+		pixels[i*4+2] = color.B
+		pixels[i*4+3] = color.A
 	}
+
+	screen.WritePixels(pixels)
 }
 
 func main() {
 	ebiten.SetWindowSize(screenWidth, screenHeight)
 	ebiten.SetWindowTitle("Mandelbrot Set")
 	game := &Game{
-		xMin:  -2,
-		xMax:  2,
-		yMin:  -2,
-		yMax:  2,
-		scale: 2,
+		xMin:       -2,
+		xMax:       2,
+		yMin:       -2,
+		yMax:       2,
+		scale:      2,
+		linearStep: 0.1,
+		zoomSpeed:  0.05,
 	}
 	if err := ebiten.RunGame(game); err != nil {
 		panic(err)
